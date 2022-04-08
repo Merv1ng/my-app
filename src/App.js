7cf27cf85c47e115d822655f0003bd7ed9a44b1f
unknown <danielvagnerqwerty@gmail.com>
2022-04-08 15:33:28 +0300

    v0.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Basket from './components/Basket/Basket';
+import Header from './components/Header/Header';
+import Info from './components/Info/Info';
+import Product from './components/Product/Product';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Info />
+
+        <div className='app-wrapper-content'>
+          <Routes>
+            <Route path="/product" element={<Product />} />
+            <Route path="/basket" element={<Basket />} />
+            
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>);
 }
 
 export default App;
+
+
+{/* <Basket />
+<Product /> */}
